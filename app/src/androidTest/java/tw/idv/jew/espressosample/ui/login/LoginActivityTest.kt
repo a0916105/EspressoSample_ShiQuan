42038fa798e714b35e81d1da991424040c83db03
a0916105 <whdofjew@gmail.com>
2021-06-23 22:55:21 +0800

    espresso的perform

--- app/src/androidTest/java/tw/idv/jew/espressosample/ui/login/LoginActivityTest.kt
+++ app/src/androidTest/java/tw/idv/jew/espressosample/ui/login/LoginActivityTest.kt
@@ -1,6 +1,7 @@
 package tw.idv.jew.espressosample.ui.login
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -17,7 +18,19 @@ class LoginActivityTest {
 
     @Test
     fun testDisableSignIn() {
+        /*Espresso.onView(ViewMatchers.withId(R.id.login))
+            .check(ViewActions.matches(not(ViewMatchers.isEnabled())))*/
         onView(withId(R.id.login))
             .check(matches(not(isEnabled())))
+    }
+
+    @Test
+    fun testEnableSignIn() {
+        onView(withId(R.id.username))
+            .perform(typeText("John"))
+        onView(withId(R.id.password))
+            .perform(typeText("mypassword"))
+        onView(withId(R.id.login))
+            .check(matches(isEnabled()))
     }
 }
